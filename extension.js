4e50f1451be9c6b8d27fe9b87644b12d1044eae0
Megh Parikh <meghprkh@users.noreply.github.com>
2020-10-30 16:26:59 +0530

    Get working in GNOME 3.38 (#13)
    Fixes #12

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
 
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
@@ -9,48 +10,50 @@ const Gtk = imports.gi.Gtk;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Meta = ExtensionUtils.getCurrentExtension();
 
+const ButtonName = "ForceQuitButton";
+
 let button;
 
-function _getCustIcon(icon_name) {
-   let gicon = Gio.icon_new_for_string( Meta.dir.get_child('icons').get_path() + "/" + icon_name + ".svg" );
-   return gicon;
-}
+const ForceQuitButton = new Lang.Class({
+    Name: ButtonName,
+    Extends: PanelMenu.Button,
+
+    _init: function () {
+        this.parent(0.0, ButtonName);
+
+        let icon = new St.Icon({
+            // icon_name: 'window-close',
+            gicon: this._getCustIcon("force-quit-symbolic"),
+        });
+
+        this.add_child(icon);
+        this.connect("button-release-event", function () {
+            GLib.spawn_command_line_async("xkill");
+        });
+    },
+
+    _getCustIcon: function (icon_name) {
+        let gicon = Gio.icon_new_for_string(
+            Meta.dir.get_child("icons").get_path() + "/" + icon_name + ".svg"
+        );
+        return gicon;
+    },
+});
 
 function init() {
-    Gtk.IconTheme.get_default().append_search_path(Meta.dir.get_child('icons').get_path());
-
-    button = new St.Bin({ name: 'force-quit',
-                          style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({
-                    // icon_name: 'window-close',
-                    gicon: _getCustIcon('force-quit-symbolic'),
-                    style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-release-event', function () {
-        GLib.spawn_command_line_async('xkill');
-    });
+    Gtk.IconTheme.get_default().append_search_path(
+        Meta.dir.get_child("icons").get_path()
+    );
 }
 
 function enable() {
-    // The default location was changed to right panel instead of left,
-    // Uncomment the following lines and comment the last line to switch back
-    // to the old behaviour
-    // let appMenu=Main.panel.statusArea.appMenu.actor.get_parent();
-    // Main.panel._leftBox.insert_child_above(button, appMenu);
-    // change this to below if you want to add it before the appmenu button
-    Main.panel._rightBox.insert_child_at_index(button, 1);
+    button = new ForceQuitButton();
+
+    // Uncomment following line to get it to the left panel besides AppMenu
+    // Main.panel.addToStatusArea(ButtonName, button, 3, 'left');
+    Main.panel.addToStatusArea(ButtonName, button);
 }
 
 function disable() {
-    // The default location was changed to right panel instead of left,
-    // Uncomment the following lines and comment the last line to switch back
-    // to the old behaviour
-    // Main.panel._leftBox.remove_child(button);
-    Main.panel._rightBox.remove_child(button);
+    button.destroy();
 }
